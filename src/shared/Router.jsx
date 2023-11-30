01c8e33eb489df16373240ea8e6c1df7d0624a7c
Solyi Park <itsoholy21@gmail.com>
2023-11-30 11:11:04 +0900

    Router 셋업

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,9 @@
-import Detail from "pages/Detail";
-import Home from "pages/Home";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Detail from 'pages/Detail';
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import Profile from 'pages/Profile';
+import SignUp from 'pages/SignUp';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function Router() {
   return (
@@ -8,6 +11,9 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />}  />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
